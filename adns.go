3faa283f3c820259f06f8a3f89b4b0e02936d07f
takezawa.tomohiro <takezawa.tomohiro@dena.jp>
2013-11-30 14:18:38 +0900

    Add server.go

--- adns.go
+++ adns.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	log.Println("started")
-	defer log.Println("stopped")
+	var (
+		isRecursive     = flag.Bool("recursive", false, "Recursive Server")
+		isAuthoritative = flag.Bool("authoritative", false, "Authoritative Server")
+		tcpFd           = flag.Int("tcpfd", -1, "TCP File Discriptor")
+		udpFd           = flag.Int("udpfd", -1, "UDP File Discriptor")
+	)
+
+	flag.Parse()
+	if flag.NArg() > 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if (*isRecursive && *isAuthoritative) || (!*isRecursive && !*isAuthoritative) {
+		fmt.Fprintln(os.Stderr, "Select either recursive or authoritative")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var err error
+	if *isRecursive {
+		// err = adns.RecursiveMain(udpFd, tcpFd)
+	}
+	if *isAuthoritative {
+		err = authoritativeMain(*udpFd, *tcpFd)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	log.Println("exited")
 }
---
+++ server.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+)
+
+func authoritativeMain(udpFd int, tcpFd int) (err error) {
+	log.Println("authoritative started")
+	return
+}
