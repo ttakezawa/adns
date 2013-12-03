4bf4686c207edcaefb63ccbb20f0fff81ef117ac
takezawa.tomohiro <takezawa.tomohiro@dena.jp>
2013-12-03 21:24:07 +0900

    Impl unpack of Header and DomainName

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "bytes"
 	"fmt"
 	"log"
 	"net"
@@ -61,6 +60,117 @@ func authoritativeMain(udpFd int, tcpFd int, udp int, tcp int) error {
 	return newError("must not come here")
 }
 
+type Walker interface {
+	// Walk iterates over fields of a structure and calls f
+	// with a reference to that field, the name of the field
+	// and a tag specifying particular encodings.
+	Walk(f func(field interface{}, name, tag string) (ok bool)) (ok bool)
+}
+
+// deserialize
+func unpackWalker(walker Walker, msg []byte, off int) (off1 int, ok bool) {
+	ok = walker.Walk(func(field interface{}, name, tag string) bool {
+		// Type switch
+		switch fv := field.(type) {
+		default:
+			log.Print("unknown packing type")
+			return false
+		case *uint16:
+			if off+2 > len(msg) {
+				return false
+			}
+			*fv = uint16(msg[off]<<8) | uint16(msg[off+1])
+			off += 2
+		case *string:
+			var s string
+			switch tag {
+			default:
+				log.Print("unknown string tag", tag)
+				return false
+			case "domain":
+				s, off, ok = unpackDomainName(msg, off)
+				if !ok {
+					log.Print("failed unpack domain name", name)
+					return false
+				}
+			}
+			*fv = s
+		}
+		return true
+	})
+	if !ok {
+		return len(msg), false
+	}
+	return off, true
+}
+
+func unpackDomainName(msg []byte, off int) (s string, off1 int, ok bool) {
+	s = ""
+	lenmsg := len(msg)
+	ptrCount := 0 // pointer follow counter
+
+	// Read all labels
+	for {
+		// Each label is represented as a one octet length field followed by that
+		// number of octets. Since every domain name ends with the null label of
+		// the root, a domain name is terminated by a length byte of zero.  The
+		// high order two bits of every length octet must be zero, and the
+		// remaining six bits of the length field limit the label to 63 octets or
+		// less.
+
+		// Read size of label
+		if off >= lenmsg {
+			return "", lenmsg, false
+		}
+		labelSize := int(msg[off])
+		off++
+
+		if labelSize == 0 {
+			break
+		}
+		if labelSize&0xC0 == 0x00 {
+			// Read a label
+			if off+labelSize > len(msg) {
+				log.Print("invalid label")
+				return "", lenmsg, false
+			}
+
+			s += string(msg[off:off+labelSize]) + "."
+			off += labelSize
+		} else if labelSize&0xC0 == 0xC0 {
+			// 上位2bitが1のときは、ポインタが指定されている
+			log.Fatal("used label is pointer")
+
+			// pointer to somewhere else in msg.
+			// remember location after first ptr,
+			// since that's how many bytes we consumed.
+			// also, don't follow too many pointers --
+			// maybe there's a loop.
+			if off >= lenmsg {
+				return "", lenmsg, false
+			}
+			leastSignificantByte := msg[off]
+			off++
+			if ptrCount == 0 {
+				off1 = off
+			}
+			if ptrCount++; ptrCount > 10 {
+				log.Print("follow too many pointers of domain name label")
+				return "", lenmsg, false
+			}
+			off = (labelSize^0xC0)<<8 | int(leastSignificantByte)
+		}
+	}
+	if s == "" {
+		s = "."
+	}
+	if ptrCount == 0 {
+		return s, off, true
+	} else {
+		return s, off1, true
+	}
+}
+
 type dnsMessage struct {
 	dnsHeader
 	Question   []dnsQuestion
@@ -69,9 +179,54 @@ type dnsMessage struct {
 	Additional []dnsRR
 }
 
+func (dns *dnsMessage) Unpack(msg []byte) (err error) {
+	off := 0
+	var ok bool
+
+	// Header
+	headerData := new(dnsHeaderData)
+	if off, ok = unpackWalker(headerData, msg, off); !ok {
+		return newError("insufficient data")
+	}
+	dns.dnsHeader.initWithData(headerData)
+
+	// Rest
+	dns.Question = make([]dnsQuestion, headerData.Qdcount)
+	dns.Answer = make([]dnsRR, headerData.Ancount)
+	dns.Authority = make([]dnsRR, headerData.Nscount)
+	dns.Additional = make([]dnsRR, headerData.Arcount)
+
+	for i := 0; i < len(dns.Question); i++ {
+		off, ok = unpackWalker(&dns.Question[i], msg, off)
+		if err != nil {
+			return
+		}
+	}
+	// TODO NOW implement
+	// for i := 0; i < len(dns.Answer); i++ {
+	// 	dns.Answer[i], off, ok = unpackRR(msg, off)
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// }
+	// for i := 0; i < len(dns.Ns); i++ {
+	// 	dns.Ns[i], off, ok = unpackRR(msg, off)
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// }
+	// for i := 0; i < len(dns.Extra); i++ {
+	// 	dns.Extra[i], off, ok = unpackRR(msg, off)
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// }
+
+	return nil
+}
+
 type dnsHeader struct {
-	Id uint16
-	// Bits uint16
+	Id     uint16
 	QR     bool // Query or Response
 	Opcode int  // OperationCode
 	AA     bool // Authoritative Answer
@@ -80,13 +235,77 @@ type dnsHeader struct {
 	RA     bool // Recursion Available
 	Z      bool // Reserved for future use. Must be zero.
 	Rcode  int  // Response code
-	// Qdcount, Ancount, Nscount, Arcount uint16
 }
+
+const (
+	// dnsHeader.Bits
+	_QR = 1 << 15 // query/response (response=1)
+	_AA = 1 << 10 // authoritative
+	_TC = 1 << 9  // truncated
+	_RD = 1 << 8  // recursion desired
+	_RA = 1 << 7  // recursion available
+)
+
+func (header *dnsHeader) initWithData(headerData *dnsHeaderData) {
+	header.Id = headerData.Id
+
+	bits := headerData.Bits
+	header.QR = (bits & _QR) != 0
+	header.Opcode = int(bits>>11) & 0xF
+	header.AA = (bits & _AA) != 0
+	header.TC = (bits & _TC) != 0
+	header.RD = (bits & _RD) != 0
+	header.RA = (bits & _RA) != 0
+	header.Rcode = int(bits & 0xF)
+}
+
+// Use like Plain Old Data
+type dnsHeaderData struct {
+	Id                                 uint16
+	Bits                               uint16
+	Qdcount, Ancount, Nscount, Arcount uint16
+}
+
+func (h *dnsHeaderData) Walk(f func(field interface{}, name, tag string) bool) bool {
+	return f(&h.Id, "Id", "") &&
+		f(&h.Bits, "Bits", "") &&
+		f(&h.Qdcount, "Qdcount", "") &&
+		f(&h.Ancount, "Ancount", "") &&
+		f(&h.Nscount, "Nscount", "") &&
+		f(&h.Arcount, "Arcount", "")
+}
+
+// // Deserialize
+// func (h *dnsHeader) Unpack(msg []byte) (qdcount uint16, ancount uint16, nscount uint16, arcount uint16, err error) {
+// 	off := 0
+
+// 	// fetch Id (16 bits)
+// 	if off+2 > len(msg) {
+// 		return newError("insufficient data")
+// 	}
+// 	h.Id = uint16(msg[off])<<8 | uint16(msg[off+1])
+// 	off += 2
+
+// 	// fetch Flags (16 bits)
+// 	if off+2 > len(msg) {
+// 		return newError("insufficient data")
+// 	}
+// 	flags := uint16(msg[off])<<8 | uint16(msg[off+1])
+// 	off += 2
+
+// 	return nil
+// }
 
 type dnsQuestion struct {
 	Qname  string
 	Qtype  uint16
 	Qclass uint16
+}
+
+func (q *dnsQuestion) Walk(f func(field interface{}, name, tag string) bool) bool {
+	return f(&q.Qname, "Qname", "domain") &&
+		f(&q.Qtype, "Qtype", "") &&
+		f(&q.Qclass, "Qclass", "")
 }
 
 type dnsRR struct {
@@ -98,11 +317,17 @@ type dnsRR struct {
 	Rdata    string
 }
 
-// func (msg *dnsMessage) Unpack(msg []byte) (err error) {
-// }
-
 func authoritativeHandleUDP(conn *net.UDPConn, remoteAddr *net.Addr, reqBytes []byte) {
 	log.Printf("Received: %d bytes\n", len(reqBytes))
+
+	reqMsg := new(dnsMessage)
+	if err := reqMsg.Unpack(reqBytes); err != nil {
+		log.Print(err)
+		conn.Close()
+		return
+	}
+
+	log.Printf("reqMsg: %#v", reqMsg)
 
 	n, err := conn.WriteTo(reqBytes, *remoteAddr)
 	if err != nil {
